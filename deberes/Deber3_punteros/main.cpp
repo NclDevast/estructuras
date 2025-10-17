851c34bc5bea800f590398c35c91a9a6903e01fe
NclDevast <santiagojove8@gmail.com>
2025-10-16 21:51:44 -0500

    estudio puntero

--- deberes/Deber3_punteros/main.cpp
+++ deberes/Deber3_punteros/main.cpp
@@ -8,6 +8,20 @@ int main(void){
     std::cout<<std::endl<<"Puntero creado con direccion: "<<ptrnum<<std::endl;
     std::cout<<"Ingrese un numero para asignar: ";
     std::cin>>*ptrnum;
-    std::cout<<"El puntero con direccion: " <<ptrnum<<std::endl<<"Al desreferenciarlo da: "<<*ptrnum;
-    delete ptrnum;
+    std::cout<<"El puntero con direccion: " <<ptrnum<<std::endl<<"Al desreferenciarlo da: "<<*ptrnum<<std::endl;
+
+    double numDouble = 5.4;
+
+    double* ptrnumDouble = new double;
+    double** ptraPtrDouble = new double*;
+
+    ptrnumDouble=&numDouble;
+    ptraPtrDouble=&ptrnumDouble;
+
+    std::cout<<"El valor de numDouble: "<<**ptraPtrDouble<<std::endl;
+    std::cout<<"Direccion en ptraPtr: "<<ptraPtrDouble<<std::endl;
+    std::cout<<"Direccion de ptrnumDouble: "<<&ptrnumDouble<<std::endl;
+    std::cout<<"Direccion de numDouble: "<<&numDouble; 
+    
+
 } 
