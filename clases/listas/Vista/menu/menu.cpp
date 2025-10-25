52edda52b154a4f1db4e1eabf460c61ba3cc7d14
Isabela2701 <isabela2701love@gmail.com>
2025-10-25 12:15:45 -0500

    Update menu.cpp

--- clases/listas/Vista/menu/menu.cpp
+++ clases/listas/Vista/menu/menu.cpp
@@ -37,19 +37,87 @@ void Menu::mostrarMenu(){
             switch (tecla){
             case 72:
             eleccion--;
-            if (eleccion >=n) eleccion=0;
+            if (eleccion < 0) eleccion = n-1;
             break;
             case 80:
             eleccion++;
-            if(eleccion >=n) eleccion=0;
+            if(eleccion >= n) eleccion=0;
             break;
         }
     }else if (tecla ==13){
         system("cls");
-        std::cout<<"Opcon sele"
+        std::cout<<"Opcion seleccionada: "<<opciones[eleccion]<<std::endl;
+    switch (eleccion){
+        case 0: 
+        insertarCabeza();
+        break;
+        case 1: 
+        insertarCola();
+        break;
+        case 2: 
+        eliminarCabeza();
+        break;
+        case 3: 
+        buscarElemento();
+        break;
+        case 4:
+        imprimirLista();
+        break;
+        case 5:
+        std::cout<<"Saliendo del programa"<<std::endl;
+        return;
+
+        default:
+        std::cout<<"Opcion invalida\n";
+    
     }
-        
-        }
+    std::cout<<"\nPresione cualquier tecla para continuar   ";
+    _getch();
 
     }
+} while (true);
+
+ }
+
+void Menu::insertarCabeza(){
+    std::cout << "Insertar Cabeza" << std::endl;
+    int valor;
+    std::cout << "Ingrese el valor a insertar: ";
+    std::cin >> valor;
+    lista.addListHead(valor);
+    std::cout << "Valor " << valor << " insertado al inicio." << std::endl;
+}
+
+void Menu::insertarCola(){
+    std::cout << "Insertar Cola" << std::endl;
+    int valor;
+    std::cout << "Ingrese el valor a insertar: ";
+    std::cin >> valor;
+    lista.addListTail(valor);
+    std::cout << "Valor " << valor << " insertado al final." << std::endl;
+}
+
+void Menu::eliminarCabeza(){
+    std::cout << "Eliminar Cola" << std::endl;
+    lista.remList();
+}
+
+void Menu::buscarElemento(){
+    std::cout << "Buscar elemento" << std::endl;
+    int valor;
+    std::cout << "Ingrese el valor a buscar: ";
+    std::cin >> valor;
+    std::cout << "Funcionalidad de busqueda en desarrollo..." << std::endl;
+}
+
+void Menu::imprimirLista(){
+    std::cout << "Imprimir lista" << std::endl;
+    lista.printList();
+}
+
+int Menu::obtenerEntero(const char* mensaje){
+    int valor;
+    std::cout << mensaje;
+    std::cin >> valor;
+    return valor;
 }
