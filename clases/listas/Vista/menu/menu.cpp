e4f4cdd5523a12f32cc545db6f17fb76b8179c43
Isabela2701 <isabela2701love@gmail.com>
2025-10-25 11:56:26 -0500

    Intento de menu con movimiento dinamico

--- clases/listas/Vista/menu/menu.cpp
+++ clases/listas/Vista/menu/menu.cpp
@@ -12,5 +12,44 @@ void Menu::mostrarMenu(){
         "Imprimir lista",
         "Salir"
     };
+    int n=6;
+    int eleccion=0;
+    int tecla;
 
+    do{
+        system("cls");
+        std::cout<<"Menu de lista enlazada simple"<<std::endl;
+        
+        int index= 0;
+        for(const auto& opcion: opciones){
+            if(index==eleccion){
+                std::cout<<" ->";
+            }else{
+                std::cout<<" ";
+            }
+            std::cout<<opcion<<std::endl;
+            index++;
+        }
+        std::cout<<"Use las flechas Arriba/Abajo para navegar y enter para elegir"<<std::endl;
+        tecla=_getch();
+        if(tecla==0||tecla==224){
+            tecla =_getch();
+            switch (tecla){
+            case 72:
+            eleccion--;
+            if (eleccion >=n) eleccion=0;
+            break;
+            case 80:
+            eleccion++;
+            if(eleccion >=n) eleccion=0;
+            break;
+        }
+    }else if (tecla ==13){
+        system("cls");
+        std::cout<<"Opcon sele"
+    }
+        
+        }
+
+    }
 }
