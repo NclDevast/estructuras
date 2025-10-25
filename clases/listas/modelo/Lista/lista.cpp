321763b9b645aaa4e84421d19ac4634a69f03488
Isabela2701 <isabela2701love@gmail.com>
2025-10-25 12:18:08 -0500

    Update lista.cpp

--- clases/listas/modelo/Lista/lista.cpp
+++ clases/listas/modelo/Lista/lista.cpp
@@ -1,4 +1,5 @@
 #include "lista.hpp"
+#include <iostream>
 
 ListaEnlazada::ListaEnlazada(){head=nullptr;}
 void ListaEnlazada::addListTail(int d){
@@ -17,4 +18,37 @@ void ListaEnlazada::addListHead(int d){
     Nodo* NewHead = new Nodo(d);
     NewHead->setSiguiente(head);
     head=NewHead;
+}
+
+void ListaEnlazada::remList(){
+    if(head == nullptr){
+        std::cout << "La lista esta vacia. No hay elementos para eliminar." << std::endl;
+        return;
+    }
+    Nodo* temp = head;
+    head = head->getSiguiente();
+    std::cout << "Elemento " << temp->getDato() << " eliminado de la cabeza." << std::endl;
+    delete temp;
+}
+
+void ListaEnlazada::printList(){
+    if(head == nullptr){
+        std::cout << "La lista esta vacia." << std::endl;
+        return;
+    }
+    Nodo* tmp = head;
+    std::cout << "Elementos de la lista: ";
+    while(tmp != nullptr){
+        std::cout << tmp->getDato() << " -> ";
+        tmp = tmp->getSiguiente();
+    }
+    std::cout << "NULL" << std::endl;
+}
+
+ListaEnlazada::~ListaEnlazada(){
+    while(head != nullptr){
+        Nodo* temp = head;
+        head = head->getSiguiente();
+        delete temp;
+    }
 }
